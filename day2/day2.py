f12d41334c15b1b967fc37482d5e6af4ae915097
efo206 <mali.976991@gmail.com>
2025-12-08 23:35:25 +0100

    added day2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -8,3 +8,29 @@ for r in ranges:
             repeats += num
 
 print(repeats)
+
+
+# part 2, highly inefficient it seems
+exclude = []
+
+for r in ranges:
+    for num in range(int(r[0]), int(r[1])+1):
+        s = str(num)
+        l = len(s)
+        for i in range(1, l):
+            if not (num in exclude):
+                if l%i == 0:
+                    splits = []
+                    ps = int(l/i)
+                    if ps != 1:
+                        phantom = s
+                        for x in range(ps):
+                            split = phantom[:i]
+                            splits.append(split)
+                            phantom = phantom[i:]
+                    if len(set(splits)) == 1: 
+                        exclude.append(num)
+                        repeats += num
+
+print(repeats)
+
